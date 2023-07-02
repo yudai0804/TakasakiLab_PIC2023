d87d50be0e3461e9fc6ee04df492afab0d45f84a
Yudai-desktop <yudai.yy0804@gmail.com>
2023-07-02 21:51:45 +0900

    rowとcolumnが逆だったので修正

--- Python/font/__init__.py
+++ Python/font/__init__.py
@@ -1,2 +1,2 @@
-from .font_converter_row_direction import *
+from .font_converter_column_direction import *
 from .font_loader import *
--- Python/matrix_animation_cli.py
+++ Python/matrix_animation_cli.py
@@ -4,10 +4,10 @@ import font
 
 f = font.FontLoader('./font/misaki_gothic_2nd.bdf')
 d = f.getDictionary()
-row_converter = font.FontConverter_RowDirection(d)
-a = row_converter.convert('  WELCOME TMCIT  ')
-# a = row_converter.convert('  産技高専へようこそ  ')
+column_converter = font.FontConverter_ColumnDirection(d)
+a = column_converter.convert('  WELCOME TMCIT  ')
+# a = column_converter.convert('  産技高専へようこそ  ')
 for i in range(10000):
-  font.viewMat8x8(row_converter.get8x8Matrix(i))
+  font.viewMat8x8(column_converter.get8x8Matrix(i))
   time.sleep(0.1)
   os.system('cls')
