4a0477f904918b90988406b8e000fe1d7ac0d165
Yamaguchi Yudai <yudai.yy0804@gmail.com>
2023-07-04 12:30:51 +0900

    円を表示できるようになった

--- Python/tkinter_led_matrix.py
+++ Python/tkinter_led_matrix.py
@@ -1,25 +1,32 @@
 import tkinter
+import math
 
 class LEDMatrix(tkinter.Canvas):
-	def __init__(self, master, canvas_width, canvas_height, radius, row, column):
-		super().__init__(master=master, width=canvas_width, height=canvas_height)
+	def __init__(self, master, radius, row, column, space_ratio = 0.1):
 		self.__radius = radius
 		self.__row = row
 		self.__column = column
-		self.__matrix = [[0] * column for i in range (row)]
+		self.__space_ratio = space_ratio
+		space = math.floor(self.__radius * self.__space_ratio)
+		self.__canvas_width = (2 * radius + space) * column + space
+		self.__canvas_height = (2 * radius + space) * row + space
+		super().__init__(master=master, width=self.__canvas_width, height=self.__canvas_height)
+		self.__matrix = [[0] * self.__column for i in range (self.__row)]
+		print(self.__matrix[0])
 		# bind
-		self.bind("<Motion>", self.onMotion)
-		self.bind("<ButtonRelease-1>", self.onClick)		
+		# self.bind("<Motion>", self.onMotion)
+		# self.bind("<ButtonRelease-1>", self.onClick)		
 		# 円を描画
-		width_offset = canvas_width / (column + 1)
-		height_offset = canvas_height / (row + 1)
-		for i in range (row):
-			for j in range (column):
+		x0 = y0 = x1 = y1 = 0
+		for i in range (self.__column):
+			y0 = y1 = 0
+			x0 = space + x1
+			x1 = x0 + 2 * radius			
+			for j in range (self.__row):
 				tag = 'led[' + str(i) + '][' + str(j) + ']'
-				x0 = width_offset*(i+1) + radius
-				y0 = height_offset*(j+1) + radius
-				x1 = width_offset*(i+1) - radius
-				y1 = height_offset*(j+1) - radius
+				y0 = space + y1
+				y1 = y0 + 2 * radius
+				# print(x0, y0, x1, y1)
 				self.create_oval(x0, y0, x1, y1, tags=tag)
 
 	def onMotion(self, event):
@@ -27,12 +34,13 @@ class LEDMatrix(tkinter.Canvas):
 	def onClick(self, event):
 		print('onClick')
 		print('x=' + str(event.x) + 'y = ' + str(event.y))
-
+	def getCanvasSize(self):
+		return self.__canvas_width, self.__canvas_height
 #ラベルの表示
 if __name__ == "__main__":
 	root = tkinter.Tk()
 	root.geometry('800x450')
-	mat = LEDMatrix(root, 300, 300, 10, 8, 8)
-	
+	mat = LEDMatrix(root, radius=16, row=10, column=8, space_ratio=0.4)
 	mat.pack()
+	print(mat.getCanvasSize())
 	root.mainloop()
