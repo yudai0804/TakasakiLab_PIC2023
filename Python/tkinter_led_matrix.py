eb9cb2ad14c84861cd04d646e41c5c9842481531
Yamaguchi Yudai <yudai.yy0804@gmail.com>
2023-07-06 12:33:57 +0900

    wip

--- Python/tkinter_led_matrix.py
+++ Python/tkinter_led_matrix.py
@@ -69,6 +69,7 @@ if __name__ == "__main__":
 	root = tkinter.Tk()
 	root.geometry('800x450')
 	s = '  WELCOME TMCIT  '
+	# s = '  /  '
 	mat_8x8 = LEDMatrix(root, radius=6, row=8, column=8, space_ratio=0.4)
 	mat_8xn = LEDMatrix(root, radius=4, row=8, column=getStringLendth(s)*8, space_ratio=0.4)
 	loader = FontLoader('./misaki_gothic_2nd.bdf')
@@ -85,13 +86,14 @@ if __name__ == "__main__":
 			self.__max_count = max_count
 			self.__count = 0
 		def onUpdate(self):
+			self.shiftMatrix()
+			root.after(100, self.onUpdate)
+		def shiftMatrix(self):
 			os.system('cls')
 			viewMat8x8(converter.get8x8Matrix_ByteInfo(self.__count))
-			# print(self.__count)
 			mat_8x8.output(converter.get8x8Matrix_BitInfo(self.__count))
 			self.__count += 1
 			self.__count %= self.__max_count
-			root.after(100, self.onUpdate)
 
 	o = OnUpdate(getStringLendth(s)*8 - 8)
 	root.after(100, o.onUpdate())
