6046dfdd674626d02f2e8dc548e8db4411b4d564
Yamaguchi Yudai <yudai.yy0804@gmail.com>
2023-07-12 15:53:12 +0900

    add 16f1938 projects

---
+++ PIC/LEDMatrix_16F1938.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LEDMatrix_16F1938.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LEDMatrix_16F1938.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ledmatrix16f1938.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ledmatrix16f1938.x.tar
