9b548c45128fb858a6cf986a320e4b7a6b9424e8
Yudai-desktop <yudai.yy0804@gmail.com>
2023-08-07 01:21:16 +0900

    PICCondeGenrator_Delayを追加

--- Python/pic_code_generator_delay.py
+++ Python/pic_code_generator_delay.py
@@ -4,23 +4,29 @@ class PICCodeGenerator_Delay:
 		self.__max_nop = max_nop
 		self.__label_name = label_name
 		self.__variable_name = variable_name
-		self.__result = ""
+		self.__result = ";Generate Failed\n"
 		self.__max_str_len = 10000
 	def __delayOneLoop(self, cycle : int, subroutine_name : str):
 		# 最適なNOPと繰り返し回数を計算
 		nop_sum = self.__max_nop
 		tmp = [-1] * 3
 		for a in range(self.__max_nop):
-			for b in range(self.__max_nop):
-					for c in range(256):
-						if(a + b >= nop_sum):
-							break
-						if(a + c * (b + 3) + 5 == cycle):
-							nop_sum = a+b
-							tmp[0] = a
-							tmp[1] = b
-							tmp[2] = c
-		# コードを生成
+				x = cycle - a - 5
+				if(x < 3):
+					break
+				for c in range(1, 256):
+					if(x % c != 0):
+						continue
+					b = x // c - 3
+					if(b < 0):
+						break
+					sum = a + b
+					if(sum < nop_sum):
+						nop_sum = sum
+						tmp[0] = a
+						tmp[1] = b
+						tmp[2] = c
+	# コードを生成
 		if(tmp[0] == -1):
 			return ""
 		result = subroutine_name + "\n"
@@ -39,20 +45,31 @@ class PICCodeGenerator_Delay:
 		# 最適なNOPと繰り返し回数を計算
 		nop_sum = self.__max_nop
 		tmp = [-1] * 5
-		for d in range(256):
-			for e in range(256):
-				for a in range(self.__max_nop):
-					for b in range(self.__max_nop):
-						for c in range(self.__max_nop):
-							if(a + b + c >= nop_sum):
-								break
-							if(cycle == a + 5 + (b + (c + 3) * e + 4) * d):
-								nop_sum = a + b + c
-								tmp[0] = a
-								tmp[1] = b
-								tmp[2] = c
-								tmp[3] = d
-								tmp[4] = e
+		for a in range(self.__max_nop):
+			x = cycle - a - 5
+			if(x < 7):
+				break
+			for d in range(1, 256):
+				if(x % d != 0):
+					continue
+				for b in range(self.__max_nop):
+					y = x // d - b - 4
+					if(y < 3):
+						break
+					for e in range(1, 256):
+						if(y % e != 0):
+							continue
+						c = y // e - 3
+						if(c < 0):
+							break
+						sum = a + b + c
+						if(sum < nop_sum):
+							nop_sum = sum
+							tmp[0] = a
+							tmp[1] = b
+							tmp[2] = c
+							tmp[3] = d
+							tmp[4] = e
 		# コードを生成
 		if(tmp[0] == -1):
 			return ""
@@ -79,25 +96,41 @@ class PICCodeGenerator_Delay:
 		# 最適なNOPと繰り返し回数を計算
 		nop_sum = self.__max_nop
 		tmp = [-1] * 7
-		for e in range(256):
-			print(e)
-			for f in range(256):
-				for g in range(256):
-					for a in range(self.__max_nop):
-						for b in range(self.__max_nop):
-							for c in range(self.__max_nop):
-								for d in range(self.__max_nop):
-									if(a + b + c + d >= nop_sum):
-										break
-									if(cycle == a + 5 + (b + 4 + (c + 4 + (d + 3) * g) * f) * e ):
-										nop_sum = a + b + c + d
-										tmp[0] = a
-										tmp[1] = b
-										tmp[2] = c
-										tmp[3] = d
-										tmp[4] = e
-										tmp[5] = f
-										tmp[6] = g
+		for a in range(self.__max_nop):
+			x = cycle - a - 5
+			if(x < 11):
+				break
+			for e in range(1, 256):
+				if(x % e != 0):
+					continue
+				for b in range(self.__max_nop):
+					y = x // e - b - 4
+					if(y < 7):
+						break
+					for f in range(1, 256):
+						if(y % f != 0):
+							continue
+						for c in range(self.__max_nop):
+							z = y // f - c - 4
+							if(z < 3):
+								break
+							for g in range(1, 256):
+								if(z % g != 0):
+									continue
+								d = z // g - 3
+								if(d < 0):
+									break
+								sum = a + b + c + d
+								if(sum < nop_sum):
+									nop_sum = sum
+									tmp[0] = a
+									tmp[1] = b
+									tmp[2] = c
+									tmp[3] = d
+									tmp[4] = e
+									tmp[5] = f
+									tmp[6] = g
+
 		# コードを生成
 		if(tmp[0] == -1):
 			return ""
@@ -123,13 +156,13 @@ class PICCodeGenerator_Delay:
 		result += "\tGOTO " + self.__label_name + "3\n"
 		result += "\tDECFSZ " + self.__variable_name + "2, F\n"
 		result += "\tGOTO " + self.__label_name + "2\n"
-		result += "\tDECFSZ " + self.__variable_name + ", F\n"
+		result += "\tDECFSZ " + self.__variable_name + "1, F\n"
 		result += "\tGOTO " + self.__label_name + "1\n"
 		result += "\tRETURN"
 		return result
 	def generateDelay(self, delay : int, delay_unit : str, subroutine_name : str):
 		if(delay <= 0):
-			self.__result = ""
+			self.__result = ";Generate Failed\n"
 			return
 		# 生成したいcycle数を計算
 		delay_ns = delay
@@ -150,8 +183,8 @@ class PICCodeGenerator_Delay:
 		res = [""] * 3
 		MAX_WEIGHT = 10000
 		res_weight = MAX_WEIGHT
-		# res[0] = self.__delayOneLoop(cycle, subroutine_name)
-		# res[1] = self.__delayTwoLoop(cycle, subroutine_name)
+		res[0] = self.__delayOneLoop(cycle, subroutine_name)
+		res[1] = self.__delayTwoLoop(cycle, subroutine_name)
 		res[2] = self.__delayThreeLoop(cycle, subroutine_name)
 		# 生成に成功しているものの中で，一番文字数が少ないものを求める
 		# ラベルは文字数に含まないように考慮している
@@ -161,7 +194,7 @@ class PICCodeGenerator_Delay:
 				res_weight = w
 		if(res_weight == MAX_WEIGHT):
 			# 生成に失敗していた場合は文字列を空にしてreturn
-			return ""
+			return ";Generate Failed\n"
 		# 同じ重みを持っているパラメーターがあればresultに代入
 		for i in range(3):
 			w = len(res[i]) - (i+2)
@@ -174,5 +207,5 @@ class PICCodeGenerator_Delay:
 
 if __name__ == '__main__':
 	pic = PICCodeGenerator_Delay(1000)
-	result = pic.generateDelay(1000, "us", "Timer")
+	result = pic.generateDelay(100000, "us", "Timer")
 	print(result)
