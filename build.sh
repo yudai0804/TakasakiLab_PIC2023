4626ae442c61003ffb303308a409a500b3fc48b8
Yudai-desktop <yudai.yy0804@gmail.com>
2023-10-01 18:44:02 +0900

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 make clean
 make
 if [ $1 = "-w" ]; then
-	java -jar "C:\Program Files (x86)\Microchip\MPLABX\v5.00\mplab_platform\mplab_ipe\ipecmd.jar" -TPPK4 -P16F1938 -F"./build/LEDMATRIX.hex" -M -W4.5
+	java -jar "C:\Program Files (x86)\Microchip\MPLABX\v5.00\mplab_platform\mplab_ipe\ipecmd.jar" -TPPK4 -P16F1938 -F"./build/LEDMATRIX.hex" -M -W4.5 -OL
 else
 	:
 fi
