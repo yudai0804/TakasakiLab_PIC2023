403ecd5cbce8e312de2d4e13d19920fd2376f34d
yudai <yudai.yy0804@gmail.com>
2023-10-08 21:31:26 +0900

    update

--- Python/src/test_matrix_animation_cli.py
+++ Python/src/test_matrix_animation_cli.py
@@ -5,7 +5,7 @@ from font_loader import *
 from led_matrix import *
 from util import *
 
-f = FontLoader('./misaki_gothic_2nd.bdf')
+f = FontLoader('../../font/misaki_gothic_2nd.bdf')
 d = f.getDictionary()
 row_converter = FontConverter_RowDirection(d)
 # a = row_converter.convert('  WELCOME TMCIT')
@@ -13,5 +13,8 @@ m = LEDMatrix(mat=row_converter.convert('  産技高専へようこそ'))
 for i in range(10000):
     printBitMatrix(m.getSplitedMatrix(column_offset=i))
     time.sleep(0.1)
+    # windowsの場合
     os.system('cls')
+    # linuxの場合
+    # os.system('clear')
 
